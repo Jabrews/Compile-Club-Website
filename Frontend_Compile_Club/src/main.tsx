002260eac12bc834f0044108a91d646b754504b4
Jacob <brewbakerjacob@gmail.com>
2025-08-20 13:27:35 -0400

    fixing react query

--- Frontend_Compile_Club/src/main.tsx
+++ Frontend_Compile_Club/src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 
 import App from './App';
 import './styles/main.scss'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ Router wraps App here */}
-      <App />
-    </BrowserRouter>
+      <BrowserRouter> 
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
