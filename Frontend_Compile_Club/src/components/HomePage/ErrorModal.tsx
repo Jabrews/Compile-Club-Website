5afbfa4c35302fb2e4f662be626a32f40f65b801
Jacob <brewbakerjacob@gmail.com>
2025-08-20 15:23:23 -0400

    fixed form

--- Frontend_Compile_Club/src/components/HomePage/ErrorModal.tsx
+++ Frontend_Compile_Club/src/components/HomePage/ErrorModal.tsx
@@ -22,7 +22,7 @@ export default function ErrorModal({ isShown, toggleIsShown, errorMessage }: Ser
 
   return (
       <motion.div
-        className="delete-modal-container"
+        className="error-modal-container"
         initial={{ y: '-100vh', opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ type: 'spring', stiffness: 80, damping: 15 }}
