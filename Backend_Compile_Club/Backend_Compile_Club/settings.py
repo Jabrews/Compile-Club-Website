aea00052971d463a84eb373a0b68678ad304c9a5
Jacob <brewbakerjacob@gmail.com>
2025-08-20 12:22:16 -0400

    postgress attempt

--- Backend_Compile_Club/Backend_Compile_Club/settings.py
+++ Backend_Compile_Club/Backend_Compile_Club/settings.py
@@ -78,13 +78,10 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {
+    'default': env.db()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
